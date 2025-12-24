05ff0fc7b30da5e0a67120049c45519e180972da
z5592322 <z5592322@ad.unsw.edu.au>
2025-12-25 00:43:30 +1100

    Bring final res down

--- src/FinalResultsPage.jsx
+++ src/FinalResultsPage.jsx
@@ -170,7 +170,7 @@ export default function FinalResultsPage({ players = [], scores = {}, currentUse
           gap: '2rem', 
           position: 'relative', 
           zIndex: 10, 
-          marginTop: '4rem',
+          marginTop: '9rem',
           animation: 'fadeInUp 0.6s ease-out'
         }}>
         
