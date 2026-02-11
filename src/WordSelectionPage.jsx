6f9096d55d91ea3292ce18fc109c540db3a57633
z5592322 <z5592322@ad.unsw.edu.au>
2026-02-12 10:00:32 +1100

    Draw

--- src/WordSelectionPage.jsx
+++ src/WordSelectionPage.jsx
@@ -1326,7 +1326,7 @@ export default function WordSelectionPage({ players = [], currentUser, onConfirm
                 onTouchEnd={stopDrawing}
                 style={{
                   width: '100%',
-                  height: '800px',
+                  height: '500px',
                   border: '3px solid rgba(255, 255, 255, 0.4)',
                   borderRadius: '15px',
                   cursor: 'crosshair',
